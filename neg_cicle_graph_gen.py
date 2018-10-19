c383cd92b99e14021794346f3f6a98da8bfeedc2
santifeliu <43893180+santifeliu@users.noreply.github.com>
2018-10-19 19:00:34 -0300

    Agrego generador de grafos con ciclos negativos, falta hacer que arranque donde se quiera

---
+++ neg_cicle_graph_gen.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from random import shuffle
+
+grafo_size = 10
+ciclo_size = 5
+sarting_index = 1
+
+
+grafo = [[0.0001 for i in range(0,grafo_size)] for j in range(0,grafo_size)]
+ciclo = [1.01 for i in range(0,ciclo_size)]
+for index, peso in enumerate(ciclo[1:]):
+	#print(str(index))
+	#print(str((index +1) % (grafo_size-1)))
+	grafo[index][(index +1) % (grafo_size-1)] = peso
+grafo[ciclo_size-1][0] = 1.01
+#print(grafo)
+#indexs = [i for i in range(0,grafo_size)]
+#shuffle(indexs)
+#grafo2 = grafo
+#for ind, new_index in enumerate(indexs):
+#	grafo[ind] = grafo2[new_index]
+print(grafo)
+with open('tests/divisas/n' + str(grafo_size) + '_c' + str(ciclo_size) + '.csv', 'w+') as f:
+	f.write(str(grafo_size) + '\n')
+	for node in grafo:
+		f.write(str(node).replace('[', '').replace(']', '').replace(',', '') + '\n')
+
