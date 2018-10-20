8a66b7255a981d53f5af90c29b77badea39306a0
santifeliu <43893180+santifeliu@users.noreply.github.com>
2018-10-20 16:21:27 -0300

    generador de willi perfect

--- tests/divisas/generador.cpp
+++ tests/divisas/generador.cpp
@@ -14,25 +14,25 @@ void generarMatriz(int N, int inicio_del_ciclo, int longitud_ciclo){
 	cout << N << " "<< inicio_del_ciclo << " "<< longitud_ciclo << " "<< endl;
 	int k = inicio_del_ciclo;
 
-	vector< vector <double> > matrix(N,vector<double> (N, 0.01));
+	vector< vector <double> > matrix(N,vector<double> (N, 0.0001));
 	int i = 1;
 	
 	while(i < longitud_ciclo){
-		matrix[k][k+1] = 2;
+		matrix[k][k+1] = 1.01;
 		i++;
 		k++;
 	}
-	matrix[k][inicio_del_ciclo] = 2;
+	matrix[k][inicio_del_ciclo] = 1.01;
 	
 	cout << "Inicio generacion de grafos: " << endl;
-	ofstream grafo (to_string(N)+to_string(inicio_del_ciclo)+to_string(longitud_ciclo)+".csv");
+	ofstream grafo (to_string(N)+"_"+to_string(inicio_del_ciclo)+"_"+to_string(longitud_ciclo)+".csv");
 	grafo << N<<endl;
 	for(size_t fil = 0; fil < N; fil++)
 	{
 		grafo << matrix[fil][0];
 		for(size_t col = 1; col < N; col++)
 		{
-			grafo << ",";
+			grafo << " ";
 			grafo << matrix[fil][col];
 		}
 		grafo << endl;
@@ -41,21 +41,14 @@ void generarMatriz(int N, int inicio_del_ciclo, int longitud_ciclo){
 	cout << "Termino generacion de grafos: " << endl;
 }
 
-int main()
-{	
-	int inicio_del_ciclo = 0;
-	int cantidad_de_nodos = 5;
-	int longitud_ciclo = 1;
-	for (int j = 1; j < cantidad_de_nodos; j++)
-	{
-		for(int h = inicio_del_ciclo ; h <= j; h++)
-		{
-			for(int l = longitud_ciclo; l <= j-h ; l++)
-			{
-				cout << "asd: "<< j-h<<endl;
-				generarMatriz(j,h,l);
-			}	
-		}
-	}
+int main(int argc, char** argv){
+	int cantidad_de_nodos = atoi(argv[1]);
+	int inicio_del_ciclo = atoi(argv[2]);
+	int longitud_ciclo = atoi(argv[3]);
+	cout << inicio_del_ciclo << " " << cantidad_de_nodos << " " << longitud_ciclo;
+	/*cin >> cantidad_de_nodos;
+	cin >> inicio_del_ciclo;
+	cin >> longitud_ciclo;*/
+    generarMatriz(cantidad_de_nodos,inicio_del_ciclo,longitud_ciclo);
 	return 0;
 }
