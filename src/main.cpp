d47f52210289d32d835218083027f2e397b1d880
Cristian Kubrak <kubrakcristian@gmail.com>
2018-10-25 18:28:20 -0300

    Limpieza de codigo

--- src/main.cpp
+++ src/main.cpp
@@ -17,44 +17,14 @@ int main(int argc, char** argv){
             coordenadas.push_back(cor);
         }
         Grafo g(coordenadas);
-        //g.imprimir();
-        //listAristas aristas = g.convert();
         listAristas particionado = g.prim();
 
-        //imprimir_agm(particionado);
         Grafo agm = Grafo(particionado, n);
         listAristas res = remover_inconsistentes(particionado, agm, 2, 2, 14, 3);
         std::vector<int> colores = descubrirConexo(res, n);
         for(int v=0; v<n; v++){
             std::cout<<colores[v]<<std::endl;
         }
-
-
-        //agm.imprimir();
-        //std::ofstream file_1("/Users/pablo2martin/Downloads/algo3TP2/build/grafo2_clu_d4_f2_desvio3.csv");
-        //file_1<< "x_1, " << "y_1," << "x_2, " << "y_2" << endl;
-        // for(int i = 0; i < res.size(); i++){
-        // 	file_1 << (g.puntos()[std::get<0>(res[i])]).x << ", "<< (g.puntos()[std::get<0>(res[i])]).y <<", " << (g.puntos()[std::get<1>(res[i])]).x << ", "<< (g.puntos()[std::get<1>(res[i])]).y << std::endl;
-        // }
-
-        /*std::ofstream file_1("/home/santiago/Documents/algo_3/algo3TP2/tests/grafos/grafo11_nodos.csv");
-        for(int i = 0; i < g.puntos().size(); i++){
-            file_1 << (g.puntos()[i]).x << ", " << (g.puntos()[i]).y << std::endl;
-        }
-        std::ofstream file_2("/home/santiago/Documents/algo_3/algo3TP2/tests/grafos/grafo11_agm.csv");
-        for(int i = 0; i <particionado.size(); i++){
-            file_2 << std::get<0>(particionado[i]) << ", " << std::get<1>(particionado[i]) << std::endl;
-        }*/
-        // imprimir_agm(res);
-        //g.imprimir_pos();
-        // g.new_node(3);
-        // g.add_edge(0,2,1);
-        // g.add_edge(0,1,7);
-        // g.add_edge(1,2,5);
-        // g.imprimir();
-        // imprimir_agm(g.kruskal());
-
-        // cat grafo1.csv | ./ejec
     }else{
         std::vector<std::vector<double>> pesos;
         int n;
